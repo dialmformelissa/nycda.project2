b13cd256bd010521822848902b85e218e0957431
Melissa Hanson <dialmformelissa@gmail.com>
2016-03-06 13:55:28 -0500

    navigation bar
    changed html,css & jquery of nav bar

--- js/fancy.js
+++ js/fancy.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	$(".left a.open").on("click", function(e) {
-  	// stop default browser behavior
-  	e.preventDefault();
-  	// get the <body>. Does it currently have a class of "open-nav"? If it does, remove the class, which will collapse the left column.' If it does not have a class of "open-nav", add it, which will expand the left column.
-  	$("body").toggleClass("open-nav");
-	});
-
-
-});
+	var bodyEl = $('body'),
+                navToggleBtn = bodyEl.find('.nav-toggle-btn');
+            
+            navToggleBtn.on('click', function(e) {
+                bodyEl.toggleClass('active-nav');
+                e.preventDefault();
+            });
+            
+            
+            
+        })();
